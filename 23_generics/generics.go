51aaf467ec4c400fac3124a65a24ab7d20693b61
suchkovdenis <ny1828@yandex.ru>
2022-08-13 23:41:56 +0300

    24 errors

--- 23_generics/generics.go
+++ 23_generics/generics.go
@@ -13,7 +13,37 @@ func MapKeys[K comparable, V any](m map[K]V) []K {
 	return r
 }
 
-// TODO realize list
+type List[T any] struct {
+	head, tail *node[T]
+}
+
+type node[T any] struct {
+	next *node[T]
+	val  T
+}
+
+func (list *List[T]) Push(v T) {
+	if list.tail == nil {
+		list.head = &node[T]{val: v}
+		list.tail = list.head
+	} else {
+		list.tail.next = &node[T]{val: v}
+		list.tail = list.tail.next
+	}
+}
+
+func (list *List[T]) AsSlice() []T {
+	ans := make([]T, 0)
+	curr := list.head
+	for {
+		if curr == nil {
+			break
+		}
+		ans = append(ans, curr.val)
+		curr = curr.next
+	}
+	return ans
+}
 
 func main() {
 	m := map[int]string{1: "2", 2: "4", 4: "8"}
@@ -21,4 +51,13 @@ func main() {
 	fmt.Println(k1)
 	k := MapKeys[int, string](m)
 	fmt.Println(k)
+
+	l := List[string]{}
+	fmt.Println(l.AsSlice())
+	l.Push("One")
+	fmt.Println(l.AsSlice())
+	l.Push("Two")
+	fmt.Println(l.AsSlice())
+	l.Push("Three")
+	fmt.Println(l.AsSlice())
 }
