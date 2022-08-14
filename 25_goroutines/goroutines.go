691942b8ec414b44914a3d7a65d4835dd0195668
suchkovdenis <ny1828@yandex.ru>
2022-08-14 16:38:58 +0300

    25-34 goroutines and channels

---
+++ 25_goroutines/goroutines.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func f(from string) {
+	for i := 0; i < 3; i++ {
+		fmt.Println(from, ":", i)
+		time.Sleep(time.Second)
+	}
+}
+
+func main() {
+	f("direct")
+
+	go f("goroutine")
+	time.Sleep(time.Second * 5)
+
+	fmt.Println("done")
+}
